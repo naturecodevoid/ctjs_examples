2c74567b74694c2417750ab302b0da7f16eb0592
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-02 14:56:12 -0700

    :zap: Move the options example to a seperate file

--- examples/emitters/follow.js
+++ examples/emitters/follow.js
@@ -6,17 +6,3 @@ ct.emitters.follow(this, 'EffectName');
 // Old particles are moved with the emitter
 // Example: In context of a Type's onStep
 ct.emitters.append(this, 'EffectName');
-
-// Options
-ct.emitters.follow(this, 'Debuff', {
-    scale: {
-        x: 0.75,
-        y: 0.75,
-    },
-    position: {
-        x: 0,
-        y: -80,
-    },
-    tint: 0xff9999,
-    depth: this.depth,
-});
---
+++ examples/emitters/options.js
@@ -0,0 +1,13 @@
+// Options
+ct.emitters.follow(this, 'Debuff', {
+    scale: {
+        x: 0.75,
+        y: 0.75,
+    },
+    position: {
+        x: 0,
+        y: -80,
+    },
+    tint: 0xff9999,
+    depth: this.depth,
+});
